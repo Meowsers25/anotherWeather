99757cf8fbba844cabc5a9487304681b495de9f2
Chris <c.lamothe25@gmail.com>
2017-07-21 14:10:52 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready()
+
+
+if (navigator.geolocation) {
+ navigator.geolocation.getCurrentPosition(function(position) {
+   var lat = position.coords.latitude;
+   var lon = position.coords.longitude;
+   $("#data").html("latitude: " + lat + "<br>longitude: " + lon);
+   //console.log(lat);
+ });
+}
