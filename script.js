2402d971791043c4a5c9c0c2e52dd5611f7a2010
Chris <c.lamothe25@gmail.com>
2017-07-23 11:13:35 -0400

    reworked script.js

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-$(document).ready()
+$(document).ready(function() {
+  var long;
+  var lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      $("#data").html("latitude: " + lat + "<br>longitude: " + long);
+    });
+  }
+
+  var api="http://api.wunderground.com/api/2ab250a47985fdce/geolookup/q/" + lat + "," + long + ".json";
+  $.getJSON(api, function(data) {
+    //alert(data.location.lat);
+    console.log(api);
+  });
+
+});
 
 
-if (navigator.geolocation) {
- navigator.geolocation.getCurrentPosition(function(position) {
-   var lat = position.coords.latitude;
-   var lon = position.coords.longitude;
-   $("#data").html("latitude: " + lat + "<br>longitude: " + lon);
-   //console.log(lat);
- });
-}
+// if (navigator.geolocation) {
+//  navigator.geolocation.getCurrentPosition(function(position) {
+//    var lat = position.coords.latitude;
+//    var lon = position.coords.longitude;
+//    $("#data").html("latitude: " + lat + "<br>longitude: " + lon);
+//    //console.log(lat);
+//  });
+// }
 // note
 
 // 2nd note
